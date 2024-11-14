a9491ed97bf45265fe3315e2f254fa6fbb0f0198
juaconsio <jconchan@outlook.com>
2024-11-14 19:43:52 -0300

    Timeline: first mockup done

--- src/components/ContactInfo.tsx
+++ src/components/ContactInfo.tsx
@@ -1,46 +1,81 @@
-import { Title, Grid, Text, Container } from '@mantine/core';
+import { Title, Grid, Text, Container, Group, Button } from '@mantine/core';
 import { Timeline } from '@mantine/core';
+import { TimelineDescription } from './description';
+import { useState } from 'react';
 const ContactInfo = () => {
+  const [active, setActive] = useState(0);
   return (
-    <Container>
-      <Grid grow justify="space-between" align="center">
-        <Grid.Col span={6}>
-          <Title order={1}>Joaquín Concha Navarro</Title>
-          <Title order={6}>Software Engineer  |  Full Stack Developer</Title>
-        </Grid.Col>
-        <Grid.Col span={6}>
-          <Title order={1}>Contactame!</Title>
-          <Title order={6}>+56 9 3414 0379</Title>
+    <Container fluid>
+
+      <Title order={1}>Joaquín Concha Navarro</Title>
+      <Container pb={10}>
+
+        <Group justify="space-between" grow>
+          <Title order={6}>Software Engineer</Title>
+          <Title order={6}>Full Stack Developer</Title>
+        </Group>
+      </Container>
+      <Container pb={10}>
+
+        <Group justify="space-between" grow>
           <Title order={6}>jconchn@uc.cl</Title>
-        </Grid.Col>
-      </Grid>
+          <Title order={6}>LinkedIn</Title>
+          <Title order={6}>Github</Title>
+        </Group>
+      </Container>
+
 
       <Grid grow justify="space-between" align="center">
         <Grid.Col span={6}>
-          <Title order={6}>Experiencia</Title>
-        </Grid.Col>
-        <Grid.Col span={6}>
-          <Timeline active={1}>
-            <Timeline.Item title="Ingeniería Civil Industrial. " >
-              <Text>Diploma Ingeniería en Computación.</Text>
-              <Text>Universidad Católica de Chile</Text>
-
+          <Timeline active={active - 1}>
+            <Timeline.Item title="Ingeniero Civil Industrial" >
+              <Group>
+                <Text fw={200}>Pontificia Universidad Católica de Chile</Text>
+                <Button variant="transparent" onClick={() => setActive(1)}>Ver más</Button>
+              </Group>
             </Timeline.Item>
-            <Timeline.Item title="Ingeniero en Software Trainee" >
-              <Text>Plaglabs</Text>
+            <Timeline.Item title="Ingeniero en Software Trainee">
+              <Group>
+                <Text fw={200}>PlagLabs</Text>
+                <Button variant="transparent" onClick={() => setActive(2)}>Ver más</Button>
+              </Group>
             </Timeline.Item>
             <Timeline.Item title="Ayudante - Diseño Detallado de Software." >
-              <Text>Universidad Católica de Chile</Text>
+              <Group>
+                <Text fw={200}>Universidad Católica de Chile</Text>
+                <Button variant="transparent" onClick={() => setActive(3)}>Ver más</Button>
+              </Group>
             </Timeline.Item>
-            <Timeline.Item title="Licenciatura en Ciencia de la Ingenería. " >
-              <Text>Diploma en Ingenería de Software.</Text>
-              <Text>Universidad Católica de Chile</Text>
+            <Timeline.Item title="Licenciatura en Ciencias de la Ingenería." >
+              <Group>
+                <Text fw={200}>Universidad Católica de Chile</Text>
+                <Button variant="transparent" onClick={() => setActive(4)}>Ver más</Button>
+              </Group>
             </Timeline.Item>
             <Timeline.Item title="Ayudante - Sistemas de Información." >
-              <Text>Universidad Católica de Chile</Text>
+              <Group>
+                <Text fw={200}>Universidad Católica de Chile</Text>
+                <Button variant="transparent" onClick={() => setActive(5)}>Ver más</Button>
+              </Group>
+            </Timeline.Item>
+            <Timeline.Item title="Ingreso a Ingenería UC." >
+              <Group>
+                <Text fw={200}>Universidad Católica de Chile</Text>
+                <Button variant="transparent" onClick={() => setActive(6)}>Ver más</Button>
+              </Group>
+            </Timeline.Item>
+            <Timeline.Item title="Enseñanza media." >
+              <Group>
+                <Text fw={200}>Instituto del Puerto de San Antonio</Text>
+                <Button variant="transparent" onClick={() => setActive(7)}>Ver más</Button>
+              </Group>
             </Timeline.Item>
           </Timeline>
         </Grid.Col>
+        <Grid.Col span={6}>
+          <Text style={{ whiteSpace: 'pre-wrap' }}>{TimelineDescription[active]}</Text>
+        </Grid.Col>
+
       </Grid>
 
     </Container>
---
+++ src/components/description.ts
@@ -0,0 +1,14 @@
+export const TimelineDescription: { [key: number]: string } = {
+  0: 'Mi experiencia profesional y académica',
+  1: 'Diploma en Ingeniería en Computación',
+  2:
+    `Desarrollé funcionalidades, corregí errores y creé pruebas unitarias en un producto de software ya en marcha blanca.
+Participé en decisiones de diseño, planificación y comunicación con clientes y usuarios.
+El proyecto termino su desarrollo y paso a fase de mantenimiento.
+`,
+  3: 'Diploma en Ingenería de Software.',
+  4: `Asistí en la evaluación y corrección de proyectos semestrales, guiando a los estudiantes en la aplicación de principios de Clean Code y patrones de diseño para mejorar la calidad y la estructura de sus desarrollos de software.`,
+  5: 'Guié y evalué proyectos semestrales enfocados en la implementación de sistemas de información en organizaciones, resaltando su importancia y relación con los procesos organizacionales. Los estudiantes aplicaron conceptos teóricos a un caso ficticio, desarrollando habilidades para el diseño de sistemas informáticos en un contexto realista.',
+  6: 'Llegué a Santiago para comenzar mi formación universitaria en una de la mejores universidades de Chile.',
+  7: 'Crecí en San Antonio y me gradué de la enseñanza media.',
+}
